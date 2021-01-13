665cac2e390c1308c029637886731a5d8f6dcffc
Bautista Tancredi <bautistatancredi@hotmail.com>
2021-01-13 15:11:09 -0300

    Adding styled components and the Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Header />
+      Hello World
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  font-size: 2.4rem;
+`;
+
+const Header = () => {
+  return (
+    <header>
+      <H1>MOVIEDB</H1>
+    </header>
+  );
+};
+
+export default Header;
